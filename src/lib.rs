4f9b4ab90141d05e5fa33cf651047a26c87d72d8
vidur2 <vmod2005@gmail.com>
2021-10-01 12:34:25 -0400

    Removed Unnesescary Elements of Cost Function

--- src/lib.rs
+++ src/lib.rs
@@ -36,7 +36,7 @@ mod tests {
         let outputs = [1u8, 1u8, 1u8, 1u8, 1u8, 0u8, 0u8, 0u8, 0u8, 0u8];
         let inputs = [(46u32, 150u32, 4u32, 1u8), (50u32, 100u32, 4u32, 1u8), (52u32, 200u32, 4u32, 1u8), (55u32, 250u32, 4u32, 1u8), (46u32, 150u32, 4u32, 1u8), (10u32, 350u32, 2u32, 0u8), (100u32, 10u32, 0u32, 0u8), (5u32, 800u32, 2u32, 0u8), (300u32, 50u32, 2u32, 0u8), (6u32, 6u32, 1u32, 0u8)];
         contract.train(inputs, outputs);
-        let prediction = contract.predict(&46u32, &150u32, &4u32, &1u8);
+        let prediction = contract.predict(&46u32, &200u32, &4u32, &1u8);
         assert_eq!(1, prediction);
     }
 }
